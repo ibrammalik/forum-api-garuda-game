e5a98f9778bcff2d2c0a2a7658421d2cd9f0c86e
ibrammalik <malikmaul1173@gmail.com>
2023-09-25 10:24:13 +0700

    continuous deployment test

--- migrations/1695455900975_create-table-likes.js
+++ migrations/1695455900975_create-table-likes.js
@@ -12,7 +12,6 @@ exports.up = (pgm) => {
   });
 
   pgm.addConstraint('likes', 'fk_likes.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
-
   pgm.addConstraint('likes', 'fk_likes.comment_id_comments.id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
 };
 
@@ -20,6 +19,5 @@ exports.down = (pgm) => {
   pgm.dropTable('likes');
 
   pgm.dropConstraint('likes', 'fk_likes.owner_users.id');
-
   pgm.dropConstraint('likes', 'fk_likes.comment_id_comments.id');
 };
