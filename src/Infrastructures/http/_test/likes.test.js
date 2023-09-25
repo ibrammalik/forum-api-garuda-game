0793295ebd7185e09d9dc6c242b71fbbdd4058e3
ibrammalik <malikmaul1173@gmail.com>
2023-09-25 08:34:38 +0700

    test[likeComment] : implement 100% test coverage

--- src/Infrastructures/http/_test/likes.test.js
+++ src/Infrastructures/http/_test/likes.test.js
@@ -66,5 +66,51 @@ describe('/threads/{threadId}/comments endpoint', () => {
       expect(response.statusCode).toEqual(200);
       expect(responseJson.status).toEqual('success');
     });
+
+    it('should response 404 when thread is not valid', async () => {
+      // Arrange
+      const server = await createServer(container);
+      const {
+        accessToken, commentId,
+      } = await ServerTestHelper.addUserThreadComment(server);
+
+      // Action
+      const response = await server.inject({
+        method: 'PUT',
+        url: `/threads/not_valid_thread_id/comments/${commentId}/likes`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat like / cancel like comment karena thread tidak valid');
+    });
+
+    it('should response 404 when comment is not valid', async () => {
+      // Arrange
+      const server = await createServer(container);
+      const {
+        accessToken, threadId, commentId,
+      } = await ServerTestHelper.addUserThreadComment(server);
+
+      // Action
+      const response = await server.inject({
+        method: 'PUT',
+        url: `/threads/${threadId}/comments/not_valid_comment_id/likes`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('tidak dapat like / cancel like comment karena comment tidak valid');
+    });
   });
 });
